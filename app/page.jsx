ab61f2a577316c8b8ff92efaa71c33d84a344f17
zlatandp <zlatandnepr@gmail.com>
2025-02-12 17:26:04 +0200

    added advantages sections

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import HeroSection from "./components/HeroSection/HeroSection"
 import FilmsInfo from "./components/FilmsInfo/FilmsInfo"
 import Protective from "./components/Protective/Protective"
 import Realiability from "./components/Reliability/Reliability"
+import AdvantageSection from "./components/Advantage/Advantage"
 
 export default function filmsLanding() {
     return (
@@ -10,6 +11,21 @@ export default function filmsLanding() {
             <FilmsInfo />
             <Protective />
             <Realiability />
+            <AdvantageSection 
+                title={<>DEINE KINDER<br /> SPIELEN AKTIV?</>}
+                bgImage={"/images/advantage_bg_1.png"}/>
+            <AdvantageSection 
+                title={'DU HAST VERSPIELTE HAUSTIERE?'}
+                bgImage={"/images/advantage_bg_2.png"}/>
+            <AdvantageSection 
+                title={'DU SPIELST AUF HÖCHSTEM LEVEL?'}
+                bgImage={"/images/advantage_bg_3.png"}/>
+            <AdvantageSection 
+                title={<>DU BIST<br /> GESCHÄFTS&shy;INHABER?</>}
+                bgImage={"/images/advantage_bg_4.png"}/>
+            <AdvantageSection 
+                title={'DU SCHÄTZT HOCHWERTIGE TECHNIK?'}
+                bgImage={"/images/advantage_bg_5.png"}/>
         </>
     )
 }
